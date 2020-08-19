56afbaa85678674d03477f83994c529609c81f12
Emmanuel <eferreyramontes@gmail.com>
2020-08-19 14:05:20 +0100

    use alias for A record

--- route53.tf
+++ route53.tf
@@ -2,10 +2,22 @@ resource "aws_route53_zone" "primary" {
   name = "and-academy.com"
 }
 
-resource "aws_route53_record" "cname_route53_record" {
+//resource "aws_route53_record" "cname_route53_record" {
+//  zone_id = aws_route53_zone.primary.zone_id
+//  name    = "and-academy.com"
+//  type    = "CNAME"
+//  ttl     = "60"
+//  records = [aws_alb.main.dns_name]
+//}
+
+resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "and-academy.com"
   type    = "A"
-  ttl     = "60"
-  records = [aws_alb.main.dns_name]
+
+  alias {
+    name                   = aws_alb.main.dns_name
+    zone_id                = aws_route53_zone.primary.zone_id
+    evaluate_target_health = true
+  }
 }
